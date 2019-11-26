b7800c7d88b73b14f8a58d6b0d9bc095a3236944
Emmanuel Ndangurura <endangurura@gmail.com>
2019-11-26 05:43:02 +0300

    using iterators

--- src/lib.rs
+++ src/lib.rs
@@ -45,18 +45,23 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-      if args.len() < 3 {
-          return Err("not enough arguments");
-      }
+  pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+      args.next();
 
-      let query = args[1].clone();
-      let filename = args[2].clone();
+      let query = match args.next() {
+          Some(arg) => arg,
+          None => return Err("Didn't get a query String"),
+      };
+
+      let filename = match args.next() {
+          Some(arg) => arg,
+          None => return Err("Didn't get a filename"),
+      };
 
       let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
   
       Ok(Config { query, filename, case_sensitive })
-  }
+    }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
